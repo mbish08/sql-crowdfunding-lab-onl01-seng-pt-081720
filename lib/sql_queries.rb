0b8b73e68aa94e4f1d282aa49d36c2aee332f2cc
Mikel Bishop <mdrew8@gmail.com>
2020-10-25 21:00:37 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,7 +8,7 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
   "SELECT projects.title, SUM(pledge.amounts) FROM projects 
-  INNER JOIN  
+  INNER JOIN pledges
     ON projects.id = pledges.project_id
     GROUP BY projects.title;"
 end

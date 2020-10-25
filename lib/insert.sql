fb28a951506f43cb180adc8920744e3347efcc3d
Mikel Bishop <mdrew8@gmail.com>
2020-10-25 20:27:53 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,32 @@
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) 
+VALUES ("desk", "office", 5000, "April 1", "December 10")
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) 
+VALUES ("pillow", "bedroom", 1000, "Jan 1", "Feb 28")
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) 
+VALUES ("socks", "clothing", 500, "Feb 1", "Feb 8")
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) 
+VALUES ("shirt", "clothing", 900, "March 15", "April 30")
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) 
+VALUES ("keyboard", "office", 2777, "July 4", "August 19")
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) 
+VALUES ("light", "home improvement", 9888, "Nov 24", "Jan 3")
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) 
+VALUES ("rowing machine", "exercise", 30000, "December 1", "Sept 11")
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) 
+VALUES ("glasses", "housewares", 1000, "May 2", "June 4")
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) 
+VALUES ("speakers", "entertainment", 50100, "June 8", "")
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) 
+VALUES
 
 
 
@@ -7,24 +36,24 @@
 
 
 
-CREATE TABLE projects (
-id INTEGER PRIMARY KEY,
-title TEXT,
-category TEXT, 
-funding_goal TEXT,
-start_date TEXT,
-end_date TEXT
-);
+-- CREATE TABLE projects (
+-- id INTEGER PRIMARY KEY,
+-- title TEXT,
+-- category TEXT, 
+-- funding_goal TEXT,
+-- start_date TEXT,
+-- end_date TEXT
+-- );
 
-CREATE TABLE users (
-id INTEGER PRIMARY KEY,
-name TEXT,
-age INTEGER
-);
+-- CREATE TABLE users (
+-- id INTEGER PRIMARY KEY,
+-- name TEXT,
+-- age INTEGER
+-- );
 
-CREATE TABLE pledges (
-id INTEGER PRIMARY KEY,
-amount INTEGER,
-user_id INTEGER,
-project_id INTEGER
-);
+-- CREATE TABLE pledges (
+-- id INTEGER PRIMARY KEY,
+-- amount INTEGER,
+-- user_id INTEGER,
+-- project_id INTEGER
+-- );

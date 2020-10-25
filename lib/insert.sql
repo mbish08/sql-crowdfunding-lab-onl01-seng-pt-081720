f21769e97db4bc6947a1eb97453efc71d07cde3f
Mikel Bishop <mdrew8@gmail.com>
2020-10-25 20:36:18 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -28,8 +28,26 @@ VALUES ("speakers", "entertainment", 50100, "June 8", "July 9");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) 
 VALUES ( "monitor", "office", 16018, "Oct 1", "Oct 31");
 
-INSERT INTO users (name, age) VALUES ("Mikel", 40);
-
+INSERT INTO users (name, age) VALUES ("Abby", 11);
+INSERT INTO users (name, age) VALUES ("Bart", 40);
+INSERT INTO users (name, age) VALUES ("Craig", 34);
+INSERT INTO users (name, age) VALUES ("Dad", 70);
+INSERT INTO users (name, age) VALUES ("Ed", 82);
+INSERT INTO users (name, age) VALUES ("Fran", 50);
+INSERT INTO users (name, age) VALUES ("Gram", 82);
+INSERT INTO users (name, age) VALUES ("Harry", 12);
+INSERT INTO users (name, age) VALUES ("Ichabod", 29);
+INSERT INTO users (name, age) VALUES ("Josh", 38);
+INSERT INTO users (name, age) VALUES ("Knox", 2);
+INSERT INTO users (name, age) VALUES ("Left", 33);
+INSERT INTO users (name, age) VALUES ("Mister", 40);
+INSERT INTO users (name, age) VALUES ("Nancy", 55);
+INSERT INTO users (name, age) VALUES ("Octo", 33);
+INSERT INTO users (name, age) VALUES ("Penelope", 33);
+INSERT INTO users (name, age) VALUES ("Queenie", 37);
+INSERT INTO users (name, age) VALUES ("Randy", 46);
+INSERT INTO users (name, age) VALUES ("Santa", 100);
+INSERT INTO users (name, age) VALUES ("Terryn", 32);
 
 
 

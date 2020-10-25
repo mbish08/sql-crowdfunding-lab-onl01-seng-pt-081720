79f2216c1e1f6d169de5cbb4ec6e14838ced122d
Mikel Bishop <mdrew8@gmail.com>
2020-10-25 20:31:00 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,32 +1,34 @@
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) 
-VALUES ("desk", "office", 5000, "April 1", "December 10")
+VALUES ("desk", "office", 5000, "April 1", "December 10");
 
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) 
-VALUES ("pillow", "bedroom", 1000, "Jan 1", "Feb 28")
+VALUES ("pillow", "bedroom", 1000, "Jan 1", "Feb 28");
 
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) 
-VALUES ("socks", "clothing", 500, "Feb 1", "Feb 8")
+VALUES ("socks", "clothing", 500, "Feb 1", "Feb 8");
 
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) 
-VALUES ("shirt", "clothing", 900, "March 15", "April 30")
+VALUES ("shirt", "clothing", 900, "March 15", "April 30");
 
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) 
-VALUES ("keyboard", "office", 2777, "July 4", "August 19")
+VALUES ("keyboard", "office", 2777, "July 4", "August 19");
 
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) 
-VALUES ("light", "home improvement", 9888, "Nov 24", "Jan 3")
+VALUES ("light", "home improvement", 9888, "Nov 24", "Jan 3");
 
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) 
-VALUES ("rowing machine", "exercise", 30000, "December 1", "Sept 11")
+VALUES ("rowing machine", "exercise", 30000, "December 1", "Sept 11");
 
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) 
-VALUES ("glasses", "housewares", 1000, "May 2", "June 4")
+VALUES ("glasses", "housewares", 1000, "May 2", "June 4");
 
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) 
-VALUES ("speakers", "entertainment", 50100, "June 8", "July 9")
+VALUES ("speakers", "entertainment", 50100, "June 8", "July 9");
 
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) 
-VALUES ( "monitor", "office", 16018, "Oct 1", "Oct 31")
+VALUES ( "monitor", "office", 16018, "Oct 1", "Oct 31");
+
+INSERT INTO users (name, age) VALUES ("Mikel", 40);
 
 
 
@@ -34,16 +36,6 @@ VALUES ( "monitor", "office", 16018, "Oct 1", "Oct 31")
 
 
 
-
-
--- CREATE TABLE projects (
--- id INTEGER PRIMARY KEY,
--- title TEXT,
--- category TEXT, 
--- funding_goal TEXT,
--- start_date TEXT,
--- end_date TEXT
--- );
 
 -- CREATE TABLE users (
 -- id INTEGER PRIMARY KEY,
